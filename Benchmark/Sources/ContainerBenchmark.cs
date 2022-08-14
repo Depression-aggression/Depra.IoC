cc70582f8d85239f572631c306171f6f8bcbe70d
g0dzZz1lla <g0dzZz1lla@yandex.ru>
2022-08-15 00:50:07 +0300

    Added container benchmark

---
+++ Benchmark/Sources/ContainerBenchmark.cs
@@ -0,0 +1,79 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Validators;
+using Depra.IoC.Containers.Builders.Extensions;
+using Depra.IoC.Containers.Builders.Impl;
+using Depra.IoC.Containers.Builders.Interfaces;
+using Depra.IoC.Scope;
+
+namespace Depra.IoC.Benchmark
+{
+    [InProcess]
+    [MemoryDiagnoser]
+    [Config(typeof(Config))]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    public class ContainerBenchmark
+    {
+        private class Config : ManualConfig
+        {
+            public Config()
+            {
+                WithOptions(ConfigOptions.Default)
+                    .AddValidator(JitOptimizationsValidator.FailOnError)
+                    .AddLogger(ConsoleLogger.Default)
+                    .AddColumnProvider(DefaultColumnProviders.Instance);
+            }
+        }
+        
+        public interface IService
+        {
+        }
+
+        private class Service : IService
+        {
+        }
+
+        public class Controller
+        {
+            private readonly IService _service;
+
+            public Controller(IService service)
+            {
+                _service = service;
+            }
+        }
+
+        private readonly IScope _lambdaBased;
+        private readonly IScope _reflectionBased;
+
+        public ContainerBenchmark()
+        {
+            var lambdaBasedBuilder = new LambdaBasedContainerBuilder();
+            var reflectionBasedBuilder = new ReflectionBasedContainerBuilder();
+
+            InitContainer(lambdaBasedBuilder);
+            InitContainer(reflectionBasedBuilder);
+
+            _lambdaBased = lambdaBasedBuilder.Build().CreateScope();
+            _reflectionBased = reflectionBasedBuilder.Build().CreateScope();
+        }
+
+        [Benchmark(Baseline = true)]
+        public Controller Create() => new Controller(new Service());
+        
+        [Benchmark]
+        public Controller Lambda() => _lambdaBased.Resolve<Controller>();
+        
+        [Benchmark]
+        public Controller Reflection() => _reflectionBased.Resolve<Controller>();
+
+        private static void InitContainer(IContainerBuilder builder)
+        {
+            builder.RegisterTransient<IService, Service>()
+                .RegisterTransient<Controller>();
+        }
+    }
+}
---
+++ Benchmark/Sources/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Depra.IoC.Benchmark
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<ContainerBenchmark>();
+        }
+    }
+}
