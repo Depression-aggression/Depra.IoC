0bdc881fe33d74a227b5987eee7831e88a837622
n.melnikov <n.melnikov@nexters.com>
2024-05-07 19:12:22 +0400

    Location customization support

--- IoC.Plugins/PluginConfiguration.cs
+++ IoC.Plugins/PluginConfiguration.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.Loader;
 using Depra.IoC.Activation;
 using Depra.IoC.QoL.Builder;
@@ -14,36 +15,43 @@ namespace Depra.IoC.Plugins;
 
 public sealed class PluginConfiguration {
 	private readonly IEnumerable<Type> _filter;
+	private readonly DirectoryInfo _pluginsLocation;
 
-	public PluginConfiguration() : this([typeof(PluginAttribute)]) { }
+	public PluginConfiguration(DirectoryInfo location = null) : this(location, [typeof(PluginAttribute)]) { }
 
-	public PluginConfiguration(params Type[] filter) => _filter = filter;
+	public PluginConfiguration(DirectoryInfo location = null, params Type[] filter) : this(filter, location) { }
 
-	public PluginConfiguration(IEnumerable<Type> filter) => _filter = filter;
+	public PluginConfiguration(IEnumerable<Type> filter, DirectoryInfo location = null) {
+		_filter = filter;
+		_pluginsLocation = location ?? new FileInfo(GetType().Assembly.Location).Directory;
+	}
 
 	public IContainer Build(IActivationBuilder activation) {
 		var builder = new ContainerBuilder(activation);
-		LoadPlugins(builder);
+		LoadPlugins(_pluginsLocation, builder);
 
 		return builder.Build();
 	}
 
-	private void LoadPlugins(IContainerBuilder builder) {
-		var directory = new FileInfo(GetType().Assembly.Location).Directory;
+	private void LoadPlugins(DirectoryInfo directory, IContainerBuilder builder) {
 		var dlls = directory!.GetFiles("*.dll");
 		var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-
 		foreach (var dll in dlls.Where(x => IsSuitable(x.FullName))) {
-			var defaultContext = AssemblyLoadContext.Default;
-			var loadedAssembly = assemblies.FirstOrDefault(x =>
-				x.Location.Equals(dll.FullName, StringComparison.InvariantCultureIgnoreCase));
-
-			if (loadedAssembly == null) {
-				loadedAssembly = defaultContext.LoadFromAssemblyPath(dll.FullName);
-			}
-
+			var loadedAssembly = LoadAssembly(dll, assemblies);
 			builder.RegisterAssemblyModules(loadedAssembly);
 		}
+	}
+
+	private Assembly LoadAssembly(FileSystemInfo dll, IEnumerable<Assembly> assemblies) {
+		var defaultContext = AssemblyLoadContext.Default;
+		var loadedAssembly = assemblies.FirstOrDefault(x =>
+			x.Location.Equals(dll.FullName, StringComparison.InvariantCultureIgnoreCase));
+
+		if (loadedAssembly == null) {
+			loadedAssembly = defaultContext.LoadFromAssemblyPath(dll.FullName);
+		}
+
+		return loadedAssembly;
 	}
 
 	private bool IsSuitable(string path) => AssemblyDefinition.ReadAssembly(path)
